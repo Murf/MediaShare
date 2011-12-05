87540b9220f31d167ff6925d51e27f494b6235fb
Brett Murphy <dev@cleverweb.com.au>
2011-12-05 14:02:10 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediashare::Application.config.secret_token = 'dc925e61ba9b8f3bef2a124183781f26ada0e314d03ee1c67291a30802f60445b108721dc5e4038ed7e000178a8bf01b8654fc313a0791b21306161130d7066e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mediashare::Application.config.session_store :cookie_store, key: '_mediashare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mediashare::Application.config.session_store :active_record_store
