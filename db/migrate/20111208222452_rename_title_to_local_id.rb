dc1fbbca0e5fb74d7d86906dead5b801774dad1b
Brett Murphy <dev@cleverweb.com.au>
2011-12-09 10:31:45 +1100

    getting heroku working again

--- db/migrate/20111208222452_rename_title_to_local_id.rb
+++ db/migrate/20111208222452_rename_title_to_local_id.rb
@@ -1,6 +1,10 @@
 class RenameTitleToLocalId < ActiveRecord::Migration
-  def change
+  def up
     remove_column :mediafiles, :title
     add_column :mediafiles, :remote_id, :int, :default => 0
+  end
+  def down
+    add_column :mediafiles, :title, :string
+    remove_column :mediafiles, :remote_id
   end
 end
