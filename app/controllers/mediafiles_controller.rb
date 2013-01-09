ddfde2e4b1ac6aa648016e70752bed7811ff5c27
Brett Murphy <brett@cleverweb.com.au>
2013-01-09 17:20:52 +1100

    tidy up mediafiles view

--- app/controllers/mediafiles_controller.rb
+++ app/controllers/mediafiles_controller.rb
@@ -4,7 +4,7 @@ class MediafilesController < ApplicationController
 
   # GET /mediafiles
   def index
-    @mediafiles = Mediafile.all
+    @mediafiles = Mediafile.where(:user_id => current_user.id)
   end
 
    # GET /mediafiles/index.json
