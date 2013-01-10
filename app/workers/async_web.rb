4798d8dc2de593ca638018e155ed741712e4ccbf
Brett Murphy <brett@cleverweb.com.au>
2013-01-10 19:56:57 +1100

    use new type field of movie search result in murfs fork of imdb to filter out TV shows

--- app/workers/async_web.rb
+++ app/workers/async_web.rb
@@ -6,18 +6,21 @@ class AsyncWeb
     mediafiles.each do |mediafile|
       if (!mediafile.medium)
         search = Imdb::Search.new(File.basename(mediafile.filename, '.*').downcase.tr("_", " "))
-        if search.movies[0]
-          imdbId= search.movies[0].id
-          medium = Medium.find_by_imdb_id(imdbId)
-          if (!medium)
-            medium = Medium.new
-            medium.title = search.movies[0].title
-            #medium.genre = search.movies[0].genre   #genre not availble in this query need to find it elsewhere
-            medium.imdb_id = imdbId
-            medium.save
+        search.movies.each do |movie|
+          if movie.type == "Movie"
+            imdbId= movie.id
+            medium = Medium.find_by_imdb_id(imdbId)
+            if (!medium)
+              medium = Medium.new
+              medium.title = movie.title
+              #medium.genre = search.movies[0].genre   #genre not availble in this query need to find it elsewhere
+              medium.imdb_id = imdbId
+              medium.save
+            end
+            mediafile.medium = medium
+            mediafile.save
+            break
           end
-          mediafile.medium = medium
-          mediafile.save
         end
         sleep 1
       end
